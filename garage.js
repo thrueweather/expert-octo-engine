e663434c796a7b0f22ca3d39eabf081bff2f6891
Dmitriy Popov <dmitriypopov151298@gmail.com>
2023-10-09 10:29:19 +0300

    Create garage.js

---
+++ garage.js
@@ -0,0 +1,84 @@
+const data = {
+  "steps": [
+    {
+      "id": 1,
+      "task": "Choose Garage Location",
+      "completed": false
+    },
+    {
+      "id": 2,
+      "task": "Design Garage Layout",
+      "completed": false
+    },
+    {
+      "id": 3,
+      "task": "Obtain Building Permits",
+      "completed": false
+    },
+    {
+      "id": 4,
+      "task": "Prepare the Ground",
+      "completed": false
+    },
+    {
+      "id": 5,
+      "task": "Construct Foundation",
+      "completed": false
+    },
+    {
+      "id": 6,
+      "task": "Build Walls and Roof",
+      "completed": false
+    },
+    {
+      "id": 7,
+      "task": "Install Garage Door",
+      "completed": false
+    },
+    {
+      "id": 8,
+      "task": "Complete Electrical Wiring",
+      "completed": false
+    },
+    {
+      "id": 9,
+      "task": "Insulate Walls and Roof",
+      "completed": false
+    },
+    {
+      "id": 10,
+      "task": "Finish Interior Walls",
+      "completed": false
+    },
+    {
+      "id": 11,
+      "task": "Install Flooring",
+      "completed": false
+    },
+    {
+      "id": 12,
+      "task": "Paint Interior and Exterior",
+      "completed": false
+    },
+    {
+      "id": 13,
+      "task": "Install Lighting Fixtures",
+      "completed": false
+    },
+    {
+      "id": 14,
+      "task": "Add Storage Solutions",
+      "completed": false
+    },
+    {
+      "id": 15,
+      "task": "Final Inspection",
+      "completed": false
+    },
+    {
+      "id": 16,
+      "task": "Organize Garage",
+      "completed": false
+    }
+  ]
+}
